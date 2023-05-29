26468ae9d8f6e3262857605b827a9d99fff05a20
SWHL <liekkaskono@163.com>
2023-05-29 19:14:24 +0800

    Update pptx

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 # -*- encoding: utf-8 -*-
+import pandas as pd
 from pptx import Presentation
+import pptx
+
+
+def extract_table(table_value: pptx.table.Table) -> str:
+    table_list = []
+    for one_row in table_value.rows:
+        each = ''
+        for cell in one_row.cells:
+            each += cell.text_frame.text + ','
+        table_list.append(each)
+    table_df = pd.DataFrame(table_list)
+    return table_df.to_string()
 
 
 ppt_path = 'test_files/test_1.pptx'
 
+
 prs = Presentation(ppt_path)
+data = []
 for i, slide in enumerate(prs.slides):
     print(f'---------------{i}-----------------')
     for shape in slide.shapes:
-        if not shape.has_text_frame:
-            continue
+        if shape.has_text_frame:
+            pass
+        elif shape.has_table:
+            table_str = extract_table(shape.table)
+        elif shape.has_chart:
+            pass
+        else:
+            pass
 
-        print(shape.text)
+print('ok')
