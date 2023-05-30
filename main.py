8e4467d8779bd19a793566678bc17ed44cb289f3
SWHL <liekkaskono@163.com>
2023-05-30 09:56:14 +0800

    Update files

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 from pathlib import Path
 
 import pandas as pd
+from pptx.enum.shapes import MSO_SHAPE_TYPE
 import pptx
 from pptx import Presentation
 from utils import mkdir, write_txt
@@ -20,6 +21,9 @@ class ExtractPPTText():
             print(i)
             cur_page_content = []
             for shape in slide.shapes:
+                # if shape.shape_type == MSO_SHAPE_TYPE.GROUP:
+                #     pass
+
                 if shape.has_text_frame:
                     txt = self.extract_text(shape.text)
                     if txt:
@@ -29,12 +33,12 @@ class ExtractPPTText():
                     cur_page_content.append(table_str)
                 elif shape.has_chart:
                     pass
-                elif getattr(shape, 'image'):
+                elif hasattr(shape, 'image'):
                     self.save_image(shape.image, page_num=i)
                 else:
                     pass
 
-            cur_page_path = self.save_dir / f'{i}.txt'
+            cur_page_path = self.save_dir / f'{i:0>2}.txt'
             write_txt(cur_page_path, cur_page_content)
 
     @staticmethod
@@ -57,7 +61,10 @@ class ExtractPPTText():
 
     def save_image(self, img_value, page_num: int):
         img_name = img_value.filename
-        save_img_path = self.save_dir / f'{page_num}_{img_name}'
+
+        save_img_dir = self.save_dir / 'images'
+        mkdir(save_img_dir)
+        save_img_path = save_img_dir / f'{page_num}_{img_name}'
 
         img_blob = img_value.blob
         with open(save_img_path, "wb") as f:
@@ -65,7 +72,7 @@ class ExtractPPTText():
 
 
 if __name__ == '__main__':
-    ppt_path = 'test_files/平台宣贯培训第五期-奥丁&洛基.pptx'
+    ppt_path = 'test_files/简约活动策划方案汇报PPT模板.pptx'
 
     ppt_extracter = ExtractPPTText(ppt_path, save_dir='outputs')
 
